abd20cd88eb00a98df9e3f773c06fba18e93e67f
apramodya <pramodyaabeysinghe@gmail.com>
2017-10-23 13:48:47 +0530

    bug

--- v2/application/views/users/login.php
+++ v2/application/views/users/login.php
@@ -4,17 +4,24 @@
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
             <div class="jumbotron">
+                <h2 class="text-center">Please Login</h2><br>
                 <?php echo form_open('users/login'); ?>
-                <h2 class="text-center">Please Login</h2>
-                <select name="type" class="form-control">
-                    <option value="1">Admin</option>
-                    <option value="2">Student</option>
-                    <option value="3">Teacher</option>
-                    <option value="4">Parent</option>
-                    <option value="5">Non Academic</option>
-                </select>
-                <input type="text" class="form-control" name="username" placeholder="Username" required="" autofocus="" />
-                <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+                <div class="form-group">
+                    <select name="type" class="form-control">
+                        <option value="1">Admin</option>
+                        <option value="2">Student</option>
+                        <option value="3">Teacher</option>
+                        <option value="4">Parent</option>
+                        <option value="5">Non Academic</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="username" placeholder="Username" required=""
+                           autofocus=""/>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+                </div>
                 <label class="checkbox">
                     <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
                 </label>
